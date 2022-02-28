78841c5b2c11402cb0b8ac09f416de0212748710
Sean Duiser <duzer84@gmail.com>
2022-02-28 13:52:35 -0500

    Program successfully checks for character included in secret word

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('JS loaded properly')
+console.log('JS loaded properly');
+
+var gameWord = 'javascript'
+
+document.addEventListener('keydown', function(event) {
+    console.log('key was pressed -', event.key);
+    console.log('gameWord -', gameWord)
+
+    console.log('Is it included?', gameWord.includes(event.key))
+});
